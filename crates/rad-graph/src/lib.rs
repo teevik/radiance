708c989829abfcfdf81222bf9f04ec0f8ee650b0
teevik <teemu.vikoren@gmail.com>
2025-04-04 19:21:08 -0400

    impl Error trait for rad-graph::Error

--- crates/rad-graph/src/lib.rs
+++ crates/rad-graph/src/lib.rs
@@ -26,6 +26,8 @@ pub enum Error {
 	Vulkan(ash::vk::Result),
 }
 
+impl std::error::Error for Error {}
+
 impl Display for Error {
 	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
 		match self {
